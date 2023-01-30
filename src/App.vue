6e15ac73e3c06033c5aad5f7553f1cbbb8cf71c2
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 10:08:24 -0300

    home criado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <HeaderView />
+    <HomeView />
   </div>
 </template>
 
 <script>
 import "./styles/global.css";
 import HeaderView from "./components/HeaderView.vue";
+import HomeView from "./pages/HomeView.vue";
 
 export default {
   name: "App",
   components: {
     HeaderView,
+    HomeView,
   },
 };
 </script>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+    <div class="text-top">Vídeos novos toda <b>quinta</b> ás <b>20h</b></div>
+    <a href="https://youtube.com/wdevoficial" target="_blank">
+      <img
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/subscribe.svg"
+        alt="inscreva-se"
+        id="subscribe"
+    /></a>
+    <div class="text-bottom">youtube.com/wdevoficial</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style scoped>
+main {
+  background-color: var(--color-background-home);
+  justify-content: center;
+  align-items: center;
+}
+
+.text-top {
+  color: var(--color-text-light);
+  font-size: 22px;
+  margin-bottom: 20px;
+}
+
+#subscribe {
+  width: 340px;
+}
+
+.text-bottom {
+  color: var(--color-text-light);
+  font-size: 18px;
+  margin-top: 20px;
+}
+</style>
