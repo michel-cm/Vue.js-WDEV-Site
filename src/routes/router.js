6e8f20b909da8191e95b7e1812ec0d51a3d3f861
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 11:30:48 -0300

    page videos

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,11 +4,16 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 import HomeView from "@/pages/HomeView";
+import VideosView from "@/pages/VideosView";
 
 const routes = [
   {
     path: "/",
     component: HomeView,
+  },
+  {
+    path: "/videos",
+    component: VideosView,
   },
 ];
 
