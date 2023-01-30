715228fdfb5378aa26389c6f4a19953ef707a957
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 09:14:25 -0300

    menu base

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -12,6 +12,22 @@
         alt="Abrir menu"
         id="menu-button"
       />
+
+      <div id="menu-overlay"></div>
+
+      <div id="menu-items">
+        <img
+          src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"
+          alt="WDEV"
+          id="menu-logo"
+        />
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/videos">Vídeos</a></li>
+          <li><a href="/sobre">Sobre</a></li>
+          <li><a href="/contato">Contato</a></li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
@@ -43,5 +59,47 @@ nav {
 
 #menu-button {
   width: 30px;
+}
+
+#menu-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 40%;
+  height: 100vh;
+  background-color: #000;
+  opacity: 0.8;
+}
+
+#menu-logo {
+  width: 110px;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+#menu-items {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: var(--color-background-nav);
+  width: 60%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+ul {
+  list-style: none;
+  text-align: center;
+}
+
+ul li {
+  margin: 20px 0px;
+}
+
+ul li a {
+  color: var(--color-text-light);
 }
 </style>
