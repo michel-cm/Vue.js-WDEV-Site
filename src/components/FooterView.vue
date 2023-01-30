bd4065338485fc9615f7b590269c2436c08d2088
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 10:27:04 -0300

    footer finalizado

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,10 +1,36 @@
 <template>
-  <footer></footer>
+  <footer>
+    <a
+      class="social-link"
+      v-for="(socialLink, index) in socialLinks"
+      :key="index"
+      :href="socialLink.link"
+      target="_blank"
+    >
+      <img
+        :src="socialLink.icon"
+        :title="socialLink.label"
+        :alt="socialLink.label"
+      />
+    </a>
+  </footer>
 </template>
 
 <script>
+import api from "@/services/api.js";
+
 export default {
   name: "FooterView",
+  data() {
+    return {
+      socialLinks: [],
+    };
+  },
+  mounted() {
+    api.get("/social-links.json").then((response) => {
+      this.socialLinks = response.data;
+    });
+  },
 };
 </script>
 
@@ -16,5 +42,20 @@ footer {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.social-link {
+  border: 2px solid var(--color-text-light);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  margin: 0 10px;
+}
+
+.social-link img {
+  width: 20px;
 }
 </style>
