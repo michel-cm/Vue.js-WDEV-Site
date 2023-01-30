75ce2a51e0efce1ee3f6c6644d988b05bbcd88ec
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-01-30 08:59:56 -0300

    base/header

---
+++ src/components/HeaderView.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <nav class="container">
+      <a href="/"
+        ><img
+          src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"
+          alt="WDEV"
+          id="logo"
+      /></a>
+      <img
+        src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg"
+        alt="Abrir menu"
+        id="menu-button"
+      />
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderView",
+};
+</script>
+
+<style scoped>
+header {
+  background-color: var(--color-background-nav);
+  height: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+#logo {
+  width: 130px;
+}
+
+#menu-button {
+  width: 30px;
+}
+</style>
